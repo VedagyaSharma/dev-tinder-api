6189a9a9a63c0a523c46a09d6ea93dda0d9f627f
Vedagya_Trainer <vedagyasharma2@gmail.com>
2026-03-13 00:57:23 +0530

    exploring app.use() order and linear middleware stack order in it

--- src/app.js
+++ src/app.js
@@ -5,11 +5,35 @@ app.use("/home", (req, res) => {
     res.send("hello from home");
 });
 
+app.use("/users/hello/2", (req, res) => {
+    res.send("abcdef");
+});
+
+app.use("/users/hello", (req, res) => {
+    res.send("hello hello hello");
+});
+
+
+// The Famous /api/:version Bug
+/*
+    The first matching route wins.So if a generic route appears 
+    before a specific route, the specific one may never run. (generic == captures / swallows)
+    Express uses a linear middleware stack, not a routing tree.
+    Every request iterates sequentially through registered handlers until a match is found
+*/
+app.get("/api/:version/users", (req, res) => {
+    res.send(`Users API version ${req.params.version}`);
+});
+
+app.get("/api/health", (req, res) => {
+    res.send("Server healthy");
+});
 
 app.use("/test", (req, res) => {
     res.send("hello from test");
 });
 
+// wild card
 app.use("/", (req, res) => {
     res.send("hello from dashboard");
 });
